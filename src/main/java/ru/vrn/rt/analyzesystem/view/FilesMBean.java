4a7449544b991313c3d3986ed2be0791e972a0ba
yaroshenko <yard0706@gmail.com>
2025-08-20 23:21:12 +0300

    add on file select handler

--- src/main/java/ru/vrn/rt/analyzesystem/view/FilesMBean.java
+++ src/main/java/ru/vrn/rt/analyzesystem/view/FilesMBean.java
@@ -1,9 +1,12 @@
 package ru.vrn.rt.analyzesystem.view;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import org.apache.commons.lang3.StringUtils;
 import org.modelmapper.ModelMapper;
+import org.primefaces.event.SelectEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -25,6 +28,7 @@ public class FilesMBean {
     private String workFolder;
 
     private List<FileRecordViewDTO> filesList = new ArrayList<>();
+    private FileRecordViewDTO selectedFile;
 
     private void updateFilesList() {
         ModelMapper modelMapper = new ModelMapper();
@@ -46,11 +50,26 @@ public class FilesMBean {
         updateFilesList();
     }
 
+    public void onRowSelect(SelectEvent<FileRecordViewDTO> event) {
+        System.out.println("on select event");
+        System.out.println("-=> "+String.valueOf(event.getObject().getFileName()));
+        FacesMessage msg = new FacesMessage("File Selected", String.valueOf(event.getObject().getFileName()));
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
     public List<FileRecordViewDTO> getFilesList() {
         return filesList;
     }
 
     public void setFilesList(List<FileRecordViewDTO> filesList) {
         this.filesList = filesList;
+    }
+
+    public FileRecordViewDTO getSelectedFile() {
+        return selectedFile;
+    }
+
+    public void setSelectedFile(FileRecordViewDTO selectedFile) {
+        this.selectedFile = selectedFile;
     }
 }
