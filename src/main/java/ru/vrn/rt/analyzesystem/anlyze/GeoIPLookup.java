a3b9e9a0e50c6af0e1f13393bba24a9391d06480
yaroshenko <yard0706@gmail.com>
2025-11-16 19:09:51 +0300

    create

---
+++ src/main/java/ru/vrn/rt/analyzesystem/anlyze/GeoIPLookup.java
@@ -0,0 +1,77 @@
+package ru.vrn.rt.analyzesystem.anlyze;
+
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.exception.AddressNotFoundException;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CityResponse;
+import com.maxmind.geoip2.record.*;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class GeoIPLookup {
+    private DatabaseReader dbReader;
+
+    public GeoIPLookup(String databasePath) throws IOException {
+        // Загрузка базы данных GeoIP2
+        File database = new File(databasePath);
+        dbReader = new DatabaseReader.Builder(database).build();
+    }
+
+    public String lookupIP(String ipAddress) {
+        try {
+            InetAddress ip = InetAddress.getByName(ipAddress);
+            CityResponse response = dbReader.city(ip);
+
+            // Получение информации о стране
+            Country country = response.getCountry();
+            String countryName = country.getName();
+//            String countryCode = country.getIsoCode();
+
+//            // Получение информации о городе
+//            City city = response.getCity();
+//            String cityName = city.getName();
+//
+//            // Получение информации о местоположении
+//            Location location = response.getLocation();
+//            Double latitude = location.getLatitude();
+//            Double longitude = location.getLongitude();
+//            String timeZone = location.getTimeZone();
+//
+//            // Получение информации о почтовом индексе
+//            Postal postal = response.getPostal();
+//            String postalCode = postal.getCode();
+//
+//            // Получение информации о провайдере
+//            String network = response.getTraits().getNetwork().toString();
+//
+//            System.out.println("=== Геолокация для IP: " + ipAddress + " ===");
+//            System.out.println("Тип IP: " + (ipAddress.contains(":") ? "IPv6" : "IPv4"));
+//            System.out.println("Страна: " + countryName + " (" + countryCode + ")");
+//            System.out.println("Город: " + cityName);
+//            System.out.println("Почтовый индекс: " + postalCode);
+//            System.out.println("Координаты: " + latitude + ", " + longitude);
+//            System.out.println("Часовой пояс: " + timeZone);
+//            System.out.println("Сеть: " + network);
+//            System.out.println();
+
+            return countryName;
+        } catch (AddressNotFoundException e) {
+            System.out.println("IP адрес " + ipAddress + " не найден в базе данных");
+        } catch (UnknownHostException e) {
+            System.out.println("Неверный формат IP адреса: " + ipAddress);
+        } catch (GeoIp2Exception | IOException e) {
+            System.out.println("Ошибка при поиске IP " + ipAddress + ": " + e.getMessage());
+        }
+        return StringUtils.EMPTY;
+    }
+
+    public void close() throws IOException {
+        if (dbReader != null) {
+            dbReader.close();
+        }
+    }
+}
